89bc948a3625deeb6428d283b858fd36b3c3ebf1
amerlon- <amerlon-@student.21-school.ru>
2018-12-21 22:21:03 +0300

    added function remake to delete \n in string

--- translate.c
+++ translate.c
@@ -6,14 +6,39 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 21:23:26 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/12/21 21:27:04 by amerlon-         ###   ########.fr       */
+/*   Updated: 2018/12/21 22:10:21 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	translate(char *map)
+static char	*remake(char *line)
 {
-	map = NULL;
+	char	*res;
+	int		i;
+	int		j;
+
+	res = ft_strnew(16);
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < 16)
+	{
+		res[i] = line[j];
+		i++;
+		j++;
+		if (line[j] == '\n')
+			j++;
+	}
+	return (res);
+}
+
+int	translate(char *line)
+{
+	char	*map;
+
+	map = remake(line);
+	printf("%s\n", map);
 	return (1);
 }
