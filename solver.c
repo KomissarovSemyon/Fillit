b2d15dd3064569b4f131aea5727c1ef0952c1c53
amerlon- <amerlon-@student.21-school.ru>
2018-12-25 21:39:19 +0300

    added recursion to main algorithm

--- solver.c
+++ solver.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solver.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfahey <cfahey@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 19:55:28 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/12/25 16:22:17 by cfahey           ###   ########.fr       */
+/*   Updated: 2018/12/25 21:38:35 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,46 @@ static void	remove_figure(char *map, int c, t_trio trio, int len)
 **	Если поставила возвращает 1, иначе 0
 */
 
-static int	is_placable(char *map, int c, t_trio trio, int len)
+static int	is_placable(t_map m, int c, t_trio trio, int count)
 {
+	int len;
+
+	len = m.len;
 	trio.c1 = ((trio.c1 / 10 * len) + (trio.c1 % 10));
 	trio.c2 = ((trio.c2 / 10 * len) + (trio.c2 % 10));
 	trio.c3 = ((trio.c3 / 10 * len) + (trio.c3 % 10));
 	if ((c < len * len && c + trio.c1 < len * len && c + trio.c2 < len * len &&
-		c + trio.c3 < len * len) && (map[c] == '.' && map[c + trio.c1] == '.' &&
-		map[c + trio.c2] == '.' && map[c + trio.c3] == '.'))
+		c + trio.c3 < len * len) && (m.map[c] == '.' && m.map[c + trio.c1] == '.' &&
+		m.map[c + trio.c2] == '.' && m.map[c + trio.c3] == '.'))
 	{
-		if (((c + trio.c1) % len == len - 1) ||
-		((c + trio.c2) % len == len - 1) || ((c + trio.c3) % len == len - 1))
-			if (((c + trio.c1) % len == 0) ||
-				((c + trio.c2) % len == 0) || ((c + trio.c3) % len == 0))
-				return (0);
+		if ((((c + trio.c1) % len == len - 1) || ((c + trio.c2) % len == len - 1)
+			|| ((c + trio.c3) % len == len - 1)) && (((c + trio.c1) % len == 0) ||
+				((c + trio.c2) % len == 0) || ((c + trio.c3) % len == 0)))
+			return (0);
 		else
 		{
-			map[c] = 'A';
-			map[trio.c1] = 'A';
-			map[trio.c2] = 'A';
-			map[trio.c3] = 'A';
+			m.map[c] = 'A' + count;
+			m.map[trio.c1] = 'A' + count;
+			m.map[trio.c2] = 'A' + count;
+			m.map[trio.c3] = 'A' + count;
 			return (1);
 		}
+	}
+	return (0);
+}
+
+static int	brute_force(t_map map, int *tetriminos, t_trio *masks, int count)
+{
+	int	i;
+
+	if (tetriminos[count] == -1)
+		return (1);
+	i = -1;
+	while (++i < map.len)
+	{
+		if (is_placable(map, ++i, masks[tetriminos[count]], count))
+			if (!brute_force(map, tetriminos, masks, count + 1))
+				remove_figure(map.map, i, masks[tetriminos[count]], map.len);
 	}
 	return (0);
 }
@@ -72,12 +90,13 @@ static int	is_placable(char *map, int c, t_trio trio, int len)
 
 void	solver(int *tetriminos, t_trio *masks)
 {
-	int		i;
-	char	*map;
+	// int		i;
+	t_map	map;
 	int		len;
 
-	i = 0;
 	len = tetriminos_count(tetriminos);
-	map = map_create(size_map(len));
-	while (map[i])
+	map = map_create(len);
+	brute_force(map, tetriminos, masks, 0);
+	// map = map_create(size_map(len));
+	// while (map[i])
 }
