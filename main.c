7ea04f799753b3fa51573e20bf747f343a1cd7ff
amerlon- <amerlon-@student.21-school.ru>
2018-12-26 19:33:05 +0300

    default main

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfahey <cfahey@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 18:52:52 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/12/25 12:42:17 by cfahey           ###   ########.fr       */
+/*   Updated: 2018/12/26 19:23:47 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ int	main(int argc, char **argv)
 
 	masks = NULL;
 	if (argc != 2)
-	// 	PRINT_ERROR
-	// else
+		PRINT_ERROR
+	else
 	{
-		(void ) argv;
-		fd = open("tests/test", O_RDONLY);
+		fd = open(argv[1], O_RDONLY);
 		tetriminos = reader(fd, &masks);
 		close(fd);
 		if (!tetriminos)
