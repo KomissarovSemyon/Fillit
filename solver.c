2e055a0dab234ad90d0d241955879f66f6fc5e17
amerlon- <amerlon-@student.21-school.ru>
2018-12-27 00:45:28 +0300

    almost finished

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 19:55:28 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/12/26 19:29:23 by amerlon-         ###   ########.fr       */
+/*   Updated: 2018/12/27 00:39:36 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,28 @@ static int	tetriminos_count(int *tetriminos)
 
 static void	remove_figure(char *map, int c, t_trio trio, int len)
 {
-	trio.c1 = ((trio.c1 / 10 * len) + (trio.c1 % 10));
-	trio.c2 = ((trio.c2 / 10 * len) + (trio.c2 % 10));
-	trio.c3 = ((trio.c3 / 10 * len) + (trio.c3 % 10));
+	t_trio	fig;
+
+	fig = trio_transform(trio, len, c);
 	map[c] = '.';
-	map[trio.c1] = '.';
-	map[trio.c2] = '.';
-	map[trio.c3] = '.';
+	map[fig.c1] = '.';
+	map[fig.c2] = '.';
+	map[fig.c3] = '.';
 }
 
 static void	place_fig(t_map m, int c, t_trio trio, int count)
 {
-	int len;
+	int		len;
+	t_trio	fig;
 
 	len = m.len;
-	trio.c1 = ((trio.c1 / 10 * len) + (trio.c1 % 10));
-	trio.c2 = ((trio.c2 / 10 * len) + (trio.c2 % 10));
-	trio.c3 = ((trio.c3 / 10 * len) + (trio.c3 % 10));
+	// printf("(%d, %d, %d, %d)\n", c, trio.c1, trio.c2, trio.c3);
+	fig = trio_transform(trio, len, c);
+	// printf("(%d, %d, %d, %d)\n", c, fig.c1, fig.c2, fig.c3);
 	m.map[c] = 'A' + count;
-	m.map[c + trio.c1] = 'A' + count;
-	m.map[c + trio.c2] = 'A' + count;
-	m.map[c + trio.c3] = 'A' + count;
+	m.map[fig.c1] = 'A' + count;
+	m.map[fig.c2] = 'A' + count;
+	m.map[fig.c3] = 'A' + count;
 }
 
 /*
@@ -57,18 +58,17 @@ static void	place_fig(t_map m, int c, t_trio trio, int count)
 static int	is_placable(t_map m, int c, t_trio trio)
 {
 	int len;
+	t_trio	fig;
 
 	len = m.len;
-	trio.c1 = ((trio.c1 / 10 * len) + (trio.c1 % 10));
-	trio.c2 = ((trio.c2 / 10 * len) + (trio.c2 % 10));
-	trio.c3 = ((trio.c3 / 10 * len) + (trio.c3 % 10));
-	if ((c < len * len && c + trio.c1 < len * len && c + trio.c2 < len * len &&
-		c + trio.c3 < len * len) && (m.map[c] == '.' && m.map[c + trio.c1] == '.' &&
-		m.map[c + trio.c2] == '.' && m.map[c + trio.c3] == '.'))
+	fig = trio_transform(trio, len, c);
+	if ((c < len * len && fig.c1 < len * len && fig.c2 < len * len &&
+		fig.c3 < len * len) && (m.map[c] == '.' && m.map[fig.c1] == '.' &&
+		m.map[fig.c2] == '.' && m.map[fig.c3] == '.'))
 	{
-		if ((((c + trio.c1) % len == len - 1) || ((c + trio.c2) % len == len - 1)
-			|| ((c + trio.c3) % len == len - 1)) && (((c + trio.c1) % len == 0) ||
-				((c + trio.c2) % len == 0) || ((c + trio.c3) % len == 0)))
+		if ((c % len == len - 1|| ((fig.c1) % len == len - 1) || ((fig.c2) % len == len - 1)
+			|| ((fig.c3) % len == len - 1)) && ((c % len == 0 || (fig.c1) % len == 0) ||
+				((fig.c2) % len == 0) || ((fig.c3) % len == 0)))
 			return (0);
 		else
 			return (1);
@@ -87,13 +87,13 @@ static int	brute_force(t_map map, int *tetriminos, t_trio *masks, int count)
 	{
 		if (is_placable(map, i , masks[tetriminos[count]]))
 		{
-			// printf("Before: \n");
-			// print_map(map);
-			// printf("\n");
+			printf("Before: \n");
+			print_map(map);
+			printf("\n");
 			place_fig(map, i, masks[tetriminos[count]], count);
-			// printf("After: \n");
-			// print_map(map);
-			// printf("\n");
+			printf("After: \n");
+			print_map(map);
+			printf("\n");
 			if (brute_force(map, tetriminos, masks, count + 1))
 				return (1);
 			else
@@ -127,6 +127,9 @@ void	solver(int *tetriminos, t_trio *masks)
 	}
 	// printf("solver returned: %d\n", brute_force(map, tetriminos, masks, 0));
 	free(map.map);
-	// map = map_create(size_map(len));
-	// while (map[i])
+	// while (*tetriminos != -1)
+	// {
+	// 	printf("(%d, %d, %d)\n", masks[*tetriminos].c1, masks[*tetriminos].c2, masks[*tetriminos].c3);
+	// 	tetriminos++;
+	// }
 }
